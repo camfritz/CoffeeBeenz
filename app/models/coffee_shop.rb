3a4a91a3ef183f0b4157d432214530e7651af8bf
Nathan <hildebrn@oregonstate.edu>
2018-04-07 20:07:18 -0700

    Modify coffee shop example to include tests for price and outdoor seating.

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -1,9 +1,13 @@
 class CoffeeShop
 	attr_accessor :distance
 	attr_accessor :popularity
-	def initialize(distance, popularity)
+	attr_accessor :price
+	attr_accessor :outdoorSeating
+	def initialize(distance, popularity, price, outdoorSeating)
 		@distance = distance
 		@popularity = popularity
+		@price = price
+		@outdoorSeating = outdoorSeating
 	end
 
 	def within_local_range?
@@ -12,5 +16,13 @@ class CoffeeShop
 
 	def popularity_is_acceptable?
 		return @popularity >= 10
+	end
+
+	def is_affordable?
+		return @price <= 4
+	end
+
+	def has_outdoor_seating?
+		return @outdoorSeating > 0
 	end
 end
--- spec/models/coffee_shop_spec.rb
+++ spec/models/coffee_shop_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe CoffeeShop do
-	back_porch_coffee = CoffeeShop.new(75, 11)
-	pdx_coffee = CoffeeShop.new(170, 8)
+	back_porch_coffee = CoffeeShop.new(75, 11, 2, 12)
+	pdx_coffee = CoffeeShop.new(170, 8, 5, 2)
 
 	it "considers a coffee shop that is within local range (<= 100)" do
 		expect(back_porch_coffee.within_local_range?).to be_truthy
@@ -19,5 +19,21 @@ RSpec.describe CoffeeShop do
 	it "considers a coffee shop with an unacceptable popularity (< 10)" do
 		expect(pdx_coffee.popularity_is_acceptable?).to be_falsy
 	end
+
+	it "considers a coffee shop with acceptable price (<= 4)" do
+		expect(back_porch_coffee.is_affordable?).to be_truthy
+	end
+
+	it "considers a coffee shop with acceptable price (<= 4)" do
+		expect(pdx_coffee.is_affordable?).to be_falsy
+	end
+
+	it "considers a coffee shop with outdoor seating (> 0)" do
+		expect(back_porch_coffee.has_outdoor_seating?).to be_truthy
+	end
+
+	it "considers a coffee shop with outdoor seating (> 0)" do
+		expect(pdx_coffee.has_outdoor_seating?).to be_truthy
+	end	
 
 end
