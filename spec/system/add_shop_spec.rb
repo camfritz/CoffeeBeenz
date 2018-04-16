bdc8ff74997f532258abc5bb5278b6e3ed3eee0d
Camron Fritz <camfritz@gmail.com>
2018-04-16 09:40:18 -0700

    Add tests to simulate user adding coffee shop with different atrributes and a coffee shop with zero attributes

--- spec/system/add_shop_spec.rb
+++ spec/system/add_shop_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe "adding a coffee shop", type: :system do
 		expect(page).to have_content("Backporch Coffee")
 		expect(page).to have_content("Columbia")
 	end
+
+	it "allows a user to create a coffee shop with varied attributes" do
+		visit new_coffee_shop_path
+		fill_in "Name", with: "PDX Coffee"
+		fill_in "City", with: "Portland"
+		fill_in "Beans", with: "Pelaton:Columbia\nBrasil:Brazil"
+		click_on("Create Coffee Shop")
+		visit coffee_shops_path
+		expect(page).not_to have_content("Bend")
+		expect(page).not_to have_content("Backporch Coffee")
+
+	end
+
+	it "allows a user to create a coffee shop with no attributes" do
+		visit new_coffee_shop_path
+		fill_in "Name", with: ""
+		fill_in "City", with: ""
+		fill_in "Beans", with: ""
+		click_on("Create Coffee Shop")
+		visit coffee_shops_path
+		expect(page).to be_empty
+	end
 end
