52d20b315eb2344d139ef365969d96b338c09915
Nathan <hildebrn@oregonstate.edu>
2018-04-15 23:48:25 -0700

    Refactor and get tests functioning properly

--- spec/system/add_shop_spec.rb
+++ spec/system/add_shop_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 RSpec.describe "adding a coffee shop", type: :system do
 	it "allows a user to create a coffee shop" do
-		visit new_project_path
+		visit new_coffee_shop_path
 		fill_in "Name", with: "Backporch Coffee"
 		fille_in "City", with: "Bend"
 		fill_in "Beans", with: "Pelaton:Columbia\nBrasil:Brazil"
 		click_on("Create Project")
-		visit projects_path
-		expect(page).to have_content("Backporck Coffee")
+		visit coffee_shops_path
+		expect(page).to have_content("Backporch Coffee")
 		expect(page).to have_content("Columbia")
 	end
 
--- spec/workflows/creates_shop_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-RSpec.describe CreatesCoffeeShop do
-  it "creates a project given a name" do
-    coffee_shop = CreatesCoffeeShop.new(name: "Backporch Coffee")
-    coffee_shop.build
-    expect(coffee_shop.project.name).to eq("Backporch Coffee")
-  end
-end
