394dc3b0ec93ed15a3bfd9e2231f9f11c3a9085d
Camron Fritz <camfritz@gmail.com>
2018-04-30 11:21:35 -0700

    Add drink custom matcher and unit tests

--- spec/models/drink_spec.rb
+++ spec/models/drink_spec.rb
@@ -1,10 +1,36 @@
 require 'rails_helper'
 
+#define custom matcher have_coffee_shop
+RSpec::Matchers.define :have_coffee_shop do |expected|
+	match do |actual|
+		expect(:coffee_shops).not_to be_empty
+	end
+end
+
+RSpec::Matchers.define :be_togo do |expected|
+	match do |actual|
+		expect(actual.togo).to eq(false)
+	end
+end
+
 # expect drink to respond to coffee_shops
 
 RSpec.describe Drink, type: :model do
+	let(:drink) {Drink.new}
+
 	it "expects drink to respond to coffee shops" do
-		latte = Drink.new()
-		expect(latte).to respond_to(:coffee_shops)
+		expect(drink).to respond_to(:coffee_shops)
+	end
+
+	it "expects a drink to have at least one coffee shop" do
+		expect(drink).to have_coffee_shop
+	end
+
+	it "expects a drink to not be for togo" do
+		expect(drink).not_to be_togo
+	end
+
+	it "expects a drink to not have a name" do
+		expect(drink.name).to be_nil
 	end
 end
